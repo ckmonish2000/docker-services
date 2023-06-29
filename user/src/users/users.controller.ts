919caeab63ee6ba482e47aa9f20e611e04410fd1
monish karunakaran <ckmonish2000@gmail.com>
2023-06-29 15:41:06 +0530

    chore: Validating req params and added metadata for swagger
    - validating the incoming req param using the ParseUUIDPipe.
    - added API Tag decorator to group the controller in swagger.
    - added API Response decorator to provide metadata for the given routes.

--- user/src/users/users.controller.ts
+++ user/src/users/users.controller.ts
@@ -1,29 +1,57 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  ParseUUIDPipe,
+} from '@nestjs/common';
+import { ApiTags, ApiResponse } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './user.entity';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dto/update-user.dto';
 
-@Controller('users')
+@ApiTags('User')
+@Controller('user')
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
 
   @Post()
+  @ApiResponse({
+    status: 201,
+    description: 'The user has been successfully created.',
+  })
   create(@Body() createUserDto: CreateUserDto): Promise<User> {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
+  @ApiResponse({
+    status: 200,
+    description: 'Returns list of all the users.',
+  })
   findAll(): Promise<User[]> {
     return this.usersService.findAll();
   }
 
   @Get(':uid')
-  findOne(@Param('uid') uid: string): Promise<User> {
+  @ApiResponse({
+    status: 200,
+    description: 'Returns a single user.',
+  })
+  findOne(@Param('uid', new ParseUUIDPipe()) uid: string): Promise<User> {
     return this.usersService.findOne(uid);
   }
 
   @Delete(':uid')
-  remove(@Param('uid') uid: string): Promise<void> {
+  @ApiResponse({
+    status: 200,
+    description: 'The user has been successfully removed.',
+  })
+  remove(@Param('uid', new ParseUUIDPipe()) uid: string): Promise<void> {
     return this.usersService.remove(uid);
   }
 }
